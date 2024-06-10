e0e728740169e8a121a955e31f35962db256bddf
shclDev <jsyoo.shcl@gmail.com>
2024-06-10 12:59:27 +0900

    gradient background 적용

--- app/src/main/java/com/shcl/smarthealth/MainActivity.kt
+++ app/src/main/java/com/shcl/smarthealth/MainActivity.kt
@@ -4,14 +4,28 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.drawBehind
+import androidx.compose.ui.geometry.Offset
+import androidx.compose.ui.graphics.Brush
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.RectangleShape
 import androidx.compose.ui.tooling.preview.Preview
+import com.plcoding.cryptocurrencyappyt.presentation.ui.theme.BackGroundColor
 import com.shcl.smarthealth.ui.theme.SmartHealthTheme
+import kotlin.math.PI
+import kotlin.math.cos
+import kotlin.math.min
+import kotlin.math.pow
+import kotlin.math.sin
+import kotlin.math.sqrt
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -32,13 +46,46 @@ class MainActivity : ComponentActivity() {
 
 @Composable
 fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
+    Box(
+       modifier = Modifier.fillMaxSize().gradientBackground(BackGroundColor , angle = 260f)
     )
 }
 
-@Preview(showBackground = true)
+fun Modifier.gradientBackground(colors : List<Color>, angle : Float ) = this.then(
+    Modifier.drawBehind {
+
+        // Setting the angle in radians
+        val angleRad = angle / 180f * PI
+
+        // Fractional x
+        val x = cos(angleRad).toFloat()
+
+        // Fractional y
+        val y = sin(angleRad).toFloat()
+
+        // Set the Radius and offset as shown below
+        val radius = sqrt(size.width.pow(2) + size.height.pow(2)) / 2f
+        val offset = center + Offset(x * radius, y * radius)
+
+        // Setting the exact offset
+        val exactOffset = Offset(
+            x = min(offset.x.coerceAtLeast(0f), size.width),
+            y = size.height - min(offset.y.coerceAtLeast(0f), size.height)
+        )
+
+        // Draw a rectangle with the above values
+        drawRect(
+            brush = Brush.linearGradient(
+                colors = colors,
+                start = Offset(size.width, size.height) - exactOffset,
+                end = exactOffset
+            ),
+            size = size
+        )
+    }
+)
+
+@Preview(showBackground = true , widthDp = 300 , heightDp = 300)
 @Composable
 fun GreetingPreview() {
     SmartHealthTheme {
--- app/src/main/java/com/shcl/smarthealth/presentation/ui/theme/Color.kt
+++ app/src/main/java/com/shcl/smarthealth/presentation/ui/theme/Color.kt
@@ -1,9 +1,13 @@
 package com.plcoding.cryptocurrencyappyt.presentation.ui.theme
 
-/*
+
 import androidx.compose.ui.graphics.Color
 
 val DarkGray = Color(0xFF202020)
 val MediumGray = Color(0xFF505050)
 val ColorPrimary = Color(0xFF08FF04)
-val TextWhite = Color(0xFFEEEEEE) */
+val TextWhite = Color(0xFFEEEEEE)
+val BackGroundColor = listOf(
+    Color(2,27,121),
+    Color(2,41,138),
+    Color(5,117,230))
