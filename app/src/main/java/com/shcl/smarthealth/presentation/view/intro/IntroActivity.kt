6a11a182b503d8aea817093ecc1879898361377b
shclDev <jsyoo.shcl@gmail.com>
2024-06-12 11:20:16 +0900

    test

--- app/src/main/java/com/shcl/smarthealth/presentation/view/intro/IntroActivity.kt
+++ app/src/main/java/com/shcl/smarthealth/presentation/view/intro/IntroActivity.kt
@@ -1,16 +1,22 @@
-package com.shcl.smarthealth
+package com.shcl.smarthealth.presentation.view.intro
 
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.BorderStroke
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
+import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.aspectRatio
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.material3.Button
 import androidx.compose.material3.ElevatedButton
 import androidx.compose.material3.Scaffold
@@ -29,9 +35,17 @@ import androidx.compose.ui.geometry.Offset
 import androidx.compose.ui.graphics.Brush
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.RectangleShape
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.semantics.Role.Companion.Button
+import androidx.compose.ui.semantics.Role.Companion.Image
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.shcl.smarthealth.R
 import com.shcl.smarthealth.ui.theme.BackGroundColor
 import com.shcl.smarthealth.ui.theme.SmartHealthTheme
 import kotlin.math.PI
@@ -42,43 +56,56 @@ import kotlin.math.pow
 import kotlin.math.sin
 import kotlin.math.sqrt
 
-class MainActivity : ComponentActivity() {
-    
+class IntroActivity : ComponentActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContent {
             SmartHealthTheme {
-                    MyApp(modifier = Modifier.fillMaxSize())
+                Greeting(modifier = Modifier.fillMaxSize())
             }
         }
     }
 }
 
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
+fun Greeting( modifier: Modifier = Modifier) {
 
-    var expanded by remember { mutableStateOf(false) }
+    val logo = painterResource(id = R.drawable.intro_logo)
 
     Box(
-       modifier = Modifier
-           .fillMaxSize()
-           .gradientBackground(BackGroundColor, angle = 90f)
+        modifier = Modifier
+            .fillMaxSize()
+            .gradientBackground(BackGroundColor, angle = 90f)
     ){
-        Row(modifier = Modifier.padding(12.dp)){
-            Column(modifier = modifier.padding(24.dp)) {
-                Text("Hello")
-                Text("Android")
-            }
-            Column(modifier = modifier.padding(24.dp)) {
-                Text("Hello")
-                Text("Android")
-            }
-            ElevatedButton(onClick = { expanded = !expanded }) {
-                Text(if(expanded) "show less" else "show more")
+        Box(
+            modifier = Modifier.fillMaxWidth(1f).align(Alignment.Center)
+        ){
+            Column() {
+                Text(
+                    text = stringResource(id = R.string.intro_desc1),
+                    color = Color.White,
+                    fontSize = 39.sp,
+                    textAlign = TextAlign.Center
+                )
+                Box{
+                    Image(
+                        modifier = Modifier.size(375.dp ,131.dp ),
+                        painter = logo,
+                        contentDescription = null
+                    )
+                }
+                Text(
+                    text = stringResource(id = R.string.intro_desc2),
+                    color = Color.White,
+                    fontSize = 34.sp,
+                    fontWeight = FontWeight.Bold,
+                    textAlign = TextAlign.Center
+                )
+
             }
         }
-
     }
 }
 
@@ -103,18 +130,6 @@ fun OnBoardingScreen(
 
 }
 
-@Composable
-fun MyApp(modifier: Modifier){
-    var shouldShowOnboarding by remember { mutableStateOf(true)}
-
-    Surface(modifier){
-        if(shouldShowOnboarding){
-            OnBoardingScreen(onContinuseClicked = { shouldShowOnboarding = false})
-        }else{
-            Greeting(name = "sdafd");
-        }
-    }
-}
 
 fun Modifier.gradientBackground(colors : List<Color>, angle : Float ) = this.then(
     Modifier.drawBehind {
@@ -154,14 +169,6 @@ fun Modifier.gradientBackground(colors : List<Color>, angle : Float ) = this.the
 @Composable
 fun GreetingPreview() {
     SmartHealthTheme {
-        Greeting("Android")
-    }
-}
-
-@Preview(showBackground = true , widthDp = 300 , heightDp = 300)
-@Composable
-fun SmartHealthPreview() {
-    SmartHealthTheme {
-        MyApp(Modifier.fillMaxSize())
+        Greeting()
     }
 }
--- app/src/main/java/com/shcl/smarthealth/ui/theme/Color.kt
+++ app/src/main/java/com/shcl/smarthealth/ui/theme/Color.kt
@@ -9,3 +9,8 @@ val Pink80 = Color(0xFFEFB8C8)
 val Purple40 = Color(0xFF6650a4)
 val PurpleGrey40 = Color(0xFF625b71)
 val Pink40 = Color(0xFF7D5260)
+
+val BackGroundColor = listOf(
+    Color(0xFF0575E6),
+    Color(0xFF02298A),
+    Color(0xFF021B79))
